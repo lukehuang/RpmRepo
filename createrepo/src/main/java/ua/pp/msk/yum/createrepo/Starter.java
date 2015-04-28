561503b8a45693ef5a64b839a950f95d35111741
Maksym Shkolnyi <mshkolnyi@intropro.com>
2015-04-28 19:18:08 +0300

    Added command line options

--- createrepo/src/main/java/ua/pp/msk/yum/createrepo/Starter.java
+++ createrepo/src/main/java/ua/pp/msk/yum/createrepo/Starter.java
@@ -3,14 +3,17 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package ua.pp.msk.yum.createrepo;
 
 import java.io.File;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.GnuParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Options;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import ua.pp.msk.yum.CreateRepo;
-
 
 /**
  *
@@ -19,11 +22,43 @@ import ua.pp.msk.yum.CreateRepo;
 public class Starter {
     
     private static final Logger LOG = LoggerFactory.getLogger(Starter.class);
+    private static Options opts;
+    
+    private static Options configureOptions() {
+        opts = new Options();
+        opts.addOption("d", "dir", true, "Repository directory");
+        opts.addOption("p", "rpmdir", true, "RPM packages directory to scan");
+        opts.addOption("h", "help", false, "Print usage info");
+        return opts;
+    }
+    
+    private static void printHelp() {
+        HelpFormatter formatter = new HelpFormatter();
+        formatter.printHelp("createrepo <opts>", opts);
+    }
     
     public static void main(String[] args) throws Exception {
-        
-        CreateRepo cr = new CreateRepo( new File("/tmp/testrepo"), new File("/tmp/testrepo"));
-//        LOG.debug("Executing repo creation");
+        CommandLineParser parser = new GnuParser();
+        CommandLine cmd = parser.parse(configureOptions(), args);
+        File repoDir;
+        File rpmDir;
+        if (!cmd.hasOption("d")) {
+            LOG.error("dir option is mandatory");
+            printHelp();
+            System.exit(1);
+        }
+        repoDir = new File(cmd.getOptionValue("d"));
+        if (!repoDir.exists()) {
+            LOG.warn("Specified repository directory " + cmd.getOptionValue("d") + " does not exist");
+        }
+        if (cmd.hasOption("p")) {
+            rpmDir = new File(cmd.getOptionValue("p"));
+        } else {
+            rpmDir = repoDir;
+        }
+        CreateRepo cr = new CreateRepo(rpmDir, repoDir);
+        LOG.debug("Executing repo creation");
         cr.execute();
+        LOG.info("Repository has been successfuly created");
     }
 }
