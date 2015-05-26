9e84fe12b1a3af972b8fdfa43db4b1a86187906d
Maksym Shkolnyi <mshkolnyi@intropro.com>
2015-05-26 21:20:50 +0300

    First web try

---
+++ server/src/main/java/ua/pp/msk/yum/server/TryController.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ua.pp.msk.yum.server;
+
+import javax.ejb.Stateless;
+
+
+
+/**
+ *
+ * @author maskimko
+ */
+@Stateless
+public class TryController {
+
+   
+}
---
+++ web/src/main/java/ua/pp/msk/yum/web/TryNamed.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ua.pp.msk.yum.web;
+
+import javax.inject.Named;
+import javax.enterprise.context.Dependent;
+
+/**
+ *
+ * @author maskimko
+ */
+@Named(value = "tryNamed")
+@Dependent
+public class TryNamed {
+
+    /**
+     * Creates a new instance of TryNamed
+     */
+    public TryNamed() {
+    }
+    
+}
