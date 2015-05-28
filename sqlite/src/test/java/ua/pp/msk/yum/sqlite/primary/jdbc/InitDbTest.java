7c60ad8ca21376a1e9d6eb8e51393cab378df907
Maksym Shkolnyi <mshkolnyi@intropro.com>
2015-05-28 15:29:05 +0300

    Clean up after test

--- sqlite/src/test/java/ua/pp/msk/yum/sqlite/primary/jdbc/InitDbTest.java
+++ sqlite/src/test/java/ua/pp/msk/yum/sqlite/primary/jdbc/InitDbTest.java
@@ -5,9 +5,11 @@
  */
 package ua.pp.msk.yum.sqlite.primary.jdbc;
 
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import org.junit.After;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import org.junit.Before;
@@ -18,16 +20,24 @@ import org.slf4j.LoggerFactory;
  * @author maskimko
  */
 public class InitDbTest {
-    
-    
+
+    private static final String DB_FOLDER_LOCATION = "/tmp/testrepo";
+
     public InitDbTest() {
     }
-    
+
     @Before
-    public void init(){
-        
+    public void init() {
+
+        Path parentDb = Paths.get(DB_FOLDER_LOCATION);
+        try {
+            if (!Files.exists(parentDb)) {
+                Files.createDirectories(parentDb);
+            }
+        } catch (IOException ex) {
+            LoggerFactory.getLogger(this.getClass()).error("Cannot create test directory", ex);
+        }
     }
-    
 
     /**
      * Test of run method, of class InitDb.
@@ -35,8 +45,9 @@ public class InitDbTest {
     @Test
     public void testRun() {
         System.out.println("run");
-        Path tdb = Paths.get("/tmp/testrepo/test_sqlitedb.sqlite");
-        InitDb instance = new InitDb(tdb, null, null );
+        Path tdb = Paths.get(DB_FOLDER_LOCATION, "testDb.sqlite");
+
+        InitDb instance = new InitDb(tdb, null, null);
         instance.run();
         try {
             instance.close();
@@ -46,6 +57,21 @@ public class InitDbTest {
         assertTrue(Files.exists(tdb));
     }
 
-  
-    
+    @After
+    public void shutdown() {
+        Path parentDb = Paths.get(DB_FOLDER_LOCATION);
+        Path tdb = Paths.get(DB_FOLDER_LOCATION, "testDb.sqlite");
+        try {
+              if (Files.exists(tdb)) {
+                Files.delete(tdb);
+            }
+            if (Files.exists(parentDb)) {
+                Files.delete(parentDb);
+            }
+          
+        } catch (IOException ex) {
+            LoggerFactory.getLogger(this.getClass()).error("Cannot delete file", ex);
+        }
+    }
+
 }
