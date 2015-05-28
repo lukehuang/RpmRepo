008cc4ec66e99ecb5c26c12e0732427bb4ef75a6
Edem Osmanov <eosmanov@intropro.com>
2015-05-28 20:30:16 +0300

    Fixed Resource loading in the tests

---
+++ sqlite/src/test/java/ua/pp/msk/yum/sqlite/InitDbTest.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ua.pp.msk.yum.sqlite;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import org.junit.After;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.slf4j.LoggerFactory;
+import ua.pp.msk.yum.sqlite.InitDb;
+
+/**
+ *
+ * @author maskimko
+ */
+public class InitDbTest {
+
+    private static final String DB_FOLDER_LOCATION = "/tmp/testrepo";
+
+    public InitDbTest() {
+    }
+
+    @Before
+    public void init() {
+
+        Path parentDb = Paths.get(DB_FOLDER_LOCATION);
+        try {
+            if (!Files.exists(parentDb)) {
+                Files.createDirectories(parentDb);
+            }
+        } catch (IOException ex) {
+            LoggerFactory.getLogger(this.getClass()).error("Cannot create test directory", ex);
+        }
+    }
+
+    /**
+     * Test of run method, of class InitDb.
+     */
+    @Test
+    public void testRun() {
+        System.out.println("run");
+        Path tdb = Paths.get(DB_FOLDER_LOCATION, "testDb.sqlite");
+
+        InitDb instance = new InitDb(tdb, null, null);
+        System.out.println(System.getProperty("java.class.path"));
+        instance.setResourceSqlPath("sql/primary/InitDB.sql");
+        instance.run();
+        try {
+            instance.close();
+        } catch (Exception ex) {
+            LoggerFactory.getLogger(this.getClass()).error("Cannot close db", ex);
+        }
+        assertTrue(Files.exists(tdb));
+    }
+
+    @After
+    public void shutdown() {
+        Path parentDb = Paths.get(DB_FOLDER_LOCATION);
+        Path tdb = Paths.get(DB_FOLDER_LOCATION, "testDb.sqlite");
+        try {
+              if (Files.exists(tdb)) {
+                Files.delete(tdb);
+            }
+            if (Files.exists(parentDb)) {
+                if (parentDb.toFile().list().length == 0) {
+                    Files.delete(parentDb);
+                }
+            }
+          
+        } catch (IOException ex) {
+            LoggerFactory.getLogger(this.getClass()).error("Cannot delete file", ex);
+        }
+    }
+
+}
